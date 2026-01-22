2368a1b64fecc9b848c2d0eaf2d023fc3e36491c
masdk9 <36520707+masdk9@users.noreply.github.com>
2026-01-22 23:45:45 +0530

    Update firebase-cfg.js

--- js/firebase-cfg.js
+++ js/firebase-cfg.js
@@ -1,1 +1,21 @@
-y
+/* =========================================
+   FIREBASE CONFIGURATION
+   ========================================= */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_suE-En5oIv3z04gJV5TPhlDwYyx-QFI",
+  authDomain: "masd-repo-git.firebaseapp.com",
+  projectId: "masd-repo-git",
+  storageBucket: "masd-repo-git.firebasestorage.app",
+  messagingSenderId: "317994984658",
+  appId: "1:317994984658:web:c55231ca09e70341c8f90b"
+};
+
+// Initialize Firebase
+// Ye check karta hai ki firebase pehle se load to nahi hai
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log("Firebase Connected Successfully!");
+} else {
+    firebase.app(); 
+}
